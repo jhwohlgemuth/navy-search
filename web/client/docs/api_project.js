d6547b55df15e6a0b6583a96700869c3b522cdd0
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-11-24 16:01:18 -0600

    Generated new docs

--- web/client/docs/api_project.js
+++ web/client/docs/api_project.js
@@ -2,13 +2,13 @@ define({
   "name": "Navy Search API",
   "title": "Navy Search API",
   "description": "\"The Search API for those that Serve\"",
-  "url": "https://usn.herokuapp.com/v1",
-  "sampleUrl": "https://usn.herokuapp.com/v1",
+  "url": "https://usn.herokuapp.com/v1.0",
+  "sampleUrl": "https://usn.herokuapp.com/v1.0",
   "version": "1.0.0",
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-11-18T03:34:06.433Z",
+    "time": "2016-11-24T22:01:06.017Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
