6aa14186170d0e646411fb7709ed84e7d8ce1f3a
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-11-27 18:08:57 -0600

    Cracked code for testing API (pass router to new express app). The time for testing has begun.

--- test/messages.js
+++ test/messages.js
@@ -1,17 +1,26 @@
+var express  = require('express');
 var mocha    = require('mocha');
 var chai     = require('chai');
-var chaiHttp = require('chai-http');
 var request  = require('supertest');
 var expect   = chai.expect;
 chai.use(chaiHttp);
 
-var app = require('../web/message');
+var router = require('../web/message');
+var app = express();
+app.use('/', router);
 
 var API_ROOT = 'http://localhost:5984/';
 var VERSION  = '1.0';
 
 describe(`GET /v${VERSION}/messages/NAVADMIN/:year`, function() {
     var endpoint = `${API_ROOT}v${VERSION}/messages/NAVADMIN/16`;
-    it('responds with JSON array', function() {
+    it('responds with JSON array', function(done) {
+        request(app)
+            .get('/NAVADMIN16042')
+            .expect(200)
+            .end(function(err, res) {
+                if (err) {return done(err);}
+                done();
+            });
     });
 });
