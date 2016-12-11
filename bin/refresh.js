eb66c9463e5a54b14f3eec21a0f287e715e09cf4
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-12-10 20:58:51 -0600

    Refactored refresh script

--- bin/refresh.js
+++ bin/refresh.js
@@ -8,58 +8,47 @@ var mongoose      = require('mongoose');
 var messageSchema = require('../web/data/schemas/message');
 var utils         = require('../web/message.utils');
 
+mongoose.Promise = Bluebird;
+mongoose.connect(process.env.MONGODB_URI);
+
+var db = mongoose.connection;
+var Message = mongoose.model('Message', messageSchema);
+
+var START_MESSAGE = chalk.dim('Started data refresh...');
+var DONE_MESSAGE = chalk.green.bold('COMPLETE');
+var ERROR_MESSAGE = chalk.red.bold('ERROR') + '\n\n';
+
+function getCurrentYear() {
+    var today = new Date();
+    // Return current year in YY format
+    return String(today.getFullYear()).substring(2);
+}
+
 function hasSameAttr(val) {
     return function(a, b) {
         return a[val] === b[val];
     }
 }
 
-mongoose.Promise = Bluebird;
-mongoose.connect(process.env.MONGODB_URI);
-var db = mongoose.connection;
-var Message = mongoose.model('Message', messageSchema);
-
-var today = new Date();
-var currentYear = String(today.getFullYear()).substring(2);
-var previousYear = String(Number(currentYear) - 1);
-
-var MESSAGES_ENDPOINT = 'https://usn.herokuapp.com/api/v1.0/messages/';
-
-var START_MESSAGE = chalk.dim('Started data refresh...');
-var DONE_MESSAGE = chalk.green.bold('COMPLETE') + '\n\n';
-var ERROR_MESSAGE = chalk.red.bold('ERROR') + '\n\n';
-
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     process.stdout.write(START_MESSAGE);
     var type = 'NAVADMIN';
-    var year = currentYear;
-    var url = 'http://www.public.navy.mil/bupers-npc/reference/messages/Documents/NAVADMINS/NAV2016/NAV16001.txt';
-    // request({
-    //     uri: url,
-    //     headers: {'User-Agent': 'Request-Promise'}
-    // }).then(function(txt) {
-    //     console.log(txt);
-    //     db.close();
-    // });
+    var year = getCurrentYear();
     Message.remove({type})
-        .then(() => utils.scrapeMessageData('NAVADMIN', year))
+        .then(() => utils.scrapeMessageData(type, year))
         .then((items) => {
             return Bluebird.all(_.uniqWith(items, hasSameAttr('num')).map((item) => {
                 var options = {
                     uri: item.url,
-                    headers: {'User-Agent': 'Request-Promise'},
+                    headers: {'User-Agent': 'navy-search-message-request'},
                     simple: false
                 };
-                return request(options).then((text) => {
-                    item.text = text;
-                    return item;
-                });
+                return request(options).then((text) => _.assign(item, {text}));
             }));
         })
         .then((items) => Message.create(items))
-        .then((items) => console.log(items.length))
-        .then(() => process.stdout.write(DONE_MESSAGE))
+        .then((items) => process.stdout.write(`${DONE_MESSAGE} (${items.length})\n\n`))
         .catch((err) => {
             process.stdout.write(ERROR_MESSAGE);
             console.log(err);
