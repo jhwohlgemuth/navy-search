3c0b82a31341dfda047a88f9cc36dc1688d094ae
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-12-12 20:32:23 -0600

    increased timeout to 500000

--- bin/refresh.js
+++ bin/refresh.js
@@ -46,7 +46,7 @@ function refreshMessages(type) {
                 var options = {
                     url: item.url,
                     method: 'GET',
-                    timeout: 120000,
+                    timeout: 500000,
                     simple: false,
                     headers: {'User-Agent': 'navy-search-request'}
                 };
