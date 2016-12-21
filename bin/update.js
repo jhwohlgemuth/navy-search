4721aa7bdee955c309d060647d4597d75bb00480
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-12-20 21:29:19 -0600

    Initial update functionality complete

--- bin/update.js
+++ bin/update.js
@@ -4,7 +4,6 @@ process.env.VERSION || require('dotenv').config();
 const _        = require('lodash');
 const chalk    = require('chalk');
 const Bluebird = require('bluebird');
-const request  = require('request-promise');
 const mongoose = require('mongoose');
 const utils    = require('../web/message.utils');
 const Message  = require('../web/data/schema/message');
@@ -13,8 +12,11 @@ const scrapeItems = utils.scrapeMessageData;
 const attemptRequest = utils.attemptRequest;
 const hasSameAttr = utils.hasSameAttr;
 
-const argv = require('yargs').argv;
+const argv = require('yargs')
+    .default('type', 'NAVADMIN')
+    .argv;
 const VERBOSE = argv.v;
+const type = argv.type;
 
 mongoose.Promise = Bluebird;
 mongoose.connect(process.env.MONGODB_URI);
@@ -22,15 +24,13 @@ mongoose.connect(process.env.MONGODB_URI);
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-    var type = 'NAVADMIN';
     var year = getCurrentYear();
     var scrapedItems = scrapeItems(type, year);
     var savedItems = Message.find({type, year}).exec();
     Bluebird.all([scrapedItems, savedItems])
-        .tap(() => Message.remove({type, year, num: '124'}))
-        .tap(() => Message.remove({type, year, num: '125'}))
+        // .tap(() => Message.remove({type, year, num: '041'}))
+        // .tap(() => Message.remove({type, year, num: '042'}))
         .then((data) => _.differenceWith(_.head(data), _.last(data), hasSameAttr('id')))
-        .tap(_.unary(console.log))
         .tap(printStartMessage)
         .map(attemptRequest)
         .then((items) => Message.create(items))
@@ -53,15 +53,20 @@ function printStartMessage(items) {
     var numberOfItems = items.length;
     if (numberOfItems > 0) {
         var justOne = (numberOfItems === 1);
-        console.log(chalk.cyan(`Updating ${chalk.bold(items.length)} ${chalk.bold(type)} message${justOne ? '' : 's'}...\n`));
+        console.log(chalk.cyan(`Updating ${chalk.bold(numberOfItems)} ${chalk.bold(type)} message${justOne ? '' : 's'}...\n`));
+        VERBOSE && console.log(items);
     } else {
-        console.log(chalk.green(`\nNo new messages\n`));
+        console.log(chalk.green(`No new messages\n`));
     }
 }
 
 function printDoneMessage(items) {
     if (Array.isArray(items)) {
-        var details = ` ~ ${chalk.bold(items.length)} messages added`;
+        var updatedItemsNumStr = items.slice(0)
+            .sort((a, b) => (a < b))
+            .map(_.property('num'))
+            .join(', ');
+        var details = ` ~ ${chalk.bold(items.length)} messages added (${updatedItemsNumStr})`;
         process.stdout.write(`${chalk.green.bold('COMPLETE')}${details}\n\n`);
     }
 }
