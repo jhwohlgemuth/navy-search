7f18d00dbe0d13105f61a11c7a95bea7a19ee96b
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-12-24 14:39:00 -0600

    Added eslint to project

---
+++ config/.eslintrc.js
@@ -0,0 +1,15 @@
+// .eslintrc.js
+// -> Curated selection of rules
+// -> Maximum auto-fix capability
+// -> Secure coding practices baked in
+module.exports = {
+    extends: 'omaha-prime-grade',
+    env: {
+        node: true,
+        browser: true,
+        es6: true
+    },
+    rules: {
+        'max-nested-callbacks': ['warn', 4],
+    }
+};
